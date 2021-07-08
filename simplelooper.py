f4f4452dfd6f8c5b0a605ec399507ae35ba1866e
David Husz <contact@davidhusz.com>
2021-07-08 18:01:46 +0100

    Update example file to reflect package changes

--- simplelooper.py
+++ simplelooper.py
@@ -3,8 +3,8 @@
 from looper import *
 
 song = Program()
-pre = song.add_snippet(Input(), start=Boot(), end=ButtonPress())
-snippet1 = song.add_snippet(Input(), start=pre.end, end=ButtonPress())
+pre = song.add_snippet(Input(), start=events.Boot(), end=events.ButtonPress())
+snippet1 = song.add_snippet(Input(), start=pre.end, end=events.ButtonPress())
 snippet1_loop = song.add_snippet(snippet1, start=snippet1.end, repeat=-1)
 snippet2 = song.add_snippet(Input(), snippet1.end+snippet1.dur, dur=snippet1.dur)
 snippet2_loop = song.add_snippet(snippet2, start=snippet2.end, repeat=-1)
