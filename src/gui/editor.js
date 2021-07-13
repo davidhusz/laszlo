46d558c5bad172314b2f3cd0d6d7b8749789a84a
David Husz <contact@davidhusz.com>
2021-07-13 21:59:25 +0100

    Add info panel

--- src/gui/editor.js
+++ src/gui/editor.js
@@ -7,7 +7,8 @@ window.addEventListener("DOMContentLoaded", () => {
 	let test_input = `{"program":{"title":"Woman (Oh Woman)","tracks":[{"id":"t1","name":"rhythm guitar","snippets":[{"id":"s1","name":"pre","source":{"stream":"input"},"start":{"event":"boot"},"end":{"event":"button_press"}},{"id":"s2","name":"verse guitar","source":{"stream":"input"},"start":{"ref":{"id":"s1","prop":"end"}},"end":{"event":"button_press"}},{"id":"s3","name":"chorus guitar","source":{"stream":"input"},"start":{"ref":{"id":"s2","prop":"end"}},"end":{"event":"button_press"}},{"id":"s4","source":{"ref":{"id":"s2"}},"start":{"ref":{"id":"s3","prop":"end"}}},{"id":"s5","source":{"ref":{"id":"s3"}},"start":{"ref":{"id":"s4","prop":"end"}}},{"id":"s6","name":"bridge guitar","source":{"stream":"input"},"start":{"ref":{"id":"s5","prop":"end"}},"end":{"event":"button_press"}},{"id":"s12","source":{"ref":{"id":"s3"}},"start":{"ref":{"id":"s6","prop":"end"}}}]},{"id":"t2","name":"bass","snippets":[{"id":"s7","name":"verse bass","source":{"stream":"input"},"start":{"ref":{"id":"s3","prop":"end"}},"dur":{"ref":{"id":"s2","prop":"dur"}}},{"id":"s8","name":"chorus bass","source":{"stream":"input"},"start":{"ref":{"id":"s7","prop":"end"}},"dur":{"ref":{"id":"s3","prop":"dur"}}},{"id":"s9","source":{"ref":{"id":"s8"}},"start":{"ref":{"id":"s6","prop":"end"}}}]},{"id":"t3","name":"lead guitar","snippets":[{"id":"s10","name":"chorus lead guitar","source":{"stream":"input"},"start":{"ref":{"id":"s6","prop":"end"}},"dur":{"ref":{"id":"s3","prop":"dur"}}}]}]},"version":"0.0"}`
 	program = Program.fromJSON(test_input, {
 		mixerContainer: document.querySelector("#mixer"),
-		workspaceContainer: document.querySelector("#workspace")
+		workspaceContainer: document.querySelector("#workspace"),
+		infoPanelContainer: document.querySelector("#info-panel")
 	});
 });
 
@@ -26,6 +27,10 @@ class Program {
 		if ("workspaceContainer" in options) {
 			this.workspaceContainer = options.workspaceContainer;
 			this.updateWorkspace();
+		}
+		if ("infoPanelContainer" in options) {
+			this.infoPanelContainer = options.infoPanelContainer;
+			this.updateInfoPanel();
 		}
 	}
 	
@@ -115,6 +120,10 @@ class Program {
 		}
 	}
 	
+	getSelectedSnippets() {
+		return this.snippets.filter(snippet => snippet.selected);
+	}
+	
 	clearSelection() {
 		this.snippets.forEach(snippet => snippet.selected = false);
 	}
@@ -145,6 +154,16 @@ class Program {
 		this.calculateSnippetStyles();
 		this.workspaceContainer.innerHTML = this.renderWorkspace();
 		[...this.tracks, ...this.snippets].forEach(item => item.addHandlers());
+	}
+	
+	updateInfoPanel() {
+		let selectedSnippets = this.getSelectedSnippets();
+		this.infoPanelContainer.innerHTML = `
+			<h1>
+				${selectedSnippets.length}
+				${selectedSnippets.length == 1 ? "snippet" : "snippets"} selected
+			</h1>
+		`;
 	}
 }
 
@@ -283,6 +302,7 @@ class Snippet {
 			}
 			this._selected = false;
 		}
+		this.containingProgram.updateInfoPanel();
 	}
 	
 	set indirectlySelected(value) {
@@ -306,9 +326,7 @@ class Snippet {
 			} else {
 				// if `this` is the only directly selected snippet, clear selection,
 				// otherwise clear selection and then select `this`
-				let currentlySelected =
-					this.containingProgram.snippets.filter(snippet => snippet.selected);
-				if (currentlySelected.length == 1) {
+				if (this.containingProgram.getSelectedSnippets().length == 1) {
 					this.containingProgram.clearSelection();
 				} else {
 					this.containingProgram.clearSelection();
