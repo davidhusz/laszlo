e0e548ae7f127ec06a36550685260ecdfed88dc3
David Husz <contact@davidhusz.com>
2021-07-18 17:40:34 +0100

    Add main menu buttons and implement 'new file'

--- src/gui/__init__.py
+++ src/gui/__init__.py
@@ -5,17 +5,40 @@ import os.path
 
 __all__ = ['open_editor']
 
-def open_editor():
-    with TemporaryDirectory(prefix='laszlo.') as temp_dir:
-        def script_dir(file):
-            return os.path.join(os.path.dirname(__file__), file)
-        url = copy(script_dir('editor.html'), temp_dir)
-        assets = ['editor.css', 'main.js', 'program.js', 'infopanel.js']
-        for asset in assets:
-            copy(script_dir(asset), temp_dir)
-        print(f'Created temporary directory {temp_dir}')
+
+class API:
+    def new(self):
+        open_editor(with_start=False)
+
+
+def open_editor(title = None, input = None, with_start = True):
+    # Create temporary directory
+    temp_dir = TemporaryDirectory(prefix='laszlo.')
+    # Copy over website files
+    def script_dir(file):
+        return os.path.join(os.path.dirname(__file__), file)
+    url = copy(script_dir('editor.html'), temp_dir.name)
+    js = copy(script_dir('main.js'), temp_dir.name)
+    assets = ['editor.css', 'program.js', 'infopanel.js']
+    for asset in assets:
+        copy(script_dir(asset), temp_dir.name)
+    # Append input to JS file as global variable
+    with open(js, mode='a') as file:
+        file.write(f'input = {repr(input or "")};\n')
+    # Create window
+    api = API()
+    window = webview.create_window(
+        title = f'{title or "untitled program"} - Laszlo Editor',
+        url = url,
+        js_api = api,
         # TODO: get client screen resolution, perhaps using pyautogui.size, or
         # package screeninfo, or with tkinter
         # https://stackoverflow.com/questions/3129322/how-do-i-get-monitor-resolution-in-python
-        webview.create_window('Laszlo Editor', url=url, width=1920, height=1080)
+        width = 1920,
+        height = 1080,
+        confirm_close = True
+    )
+    api.window = window
+    window.closed += temp_dir.cleanup
+    if with_start:
         webview.start()
--- src/gui/main.js
+++ src/gui/main.js
@@ -1,13 +1,23 @@
 "use strict";
 
-// Global variable for development purposes
-var program;
+// Global variables
+var input, program;
 
 window.addEventListener("DOMContentLoaded", () => {
-	let test_input = `{"program":{"title":"Woman (Oh Woman)","tracks":[{"id":"t1","name":"rhythm guitar","snippets":[{"id":"s1","name":"pre","source":{"stream":"input"},"start":{"event":"boot"},"end":{"event":"button_press"}},{"id":"s2","name":"verse guitar","source":{"stream":"input"},"start":{"ref":{"id":"s1","prop":"end"}},"end":{"event":"button_press"}},{"id":"s3","name":"chorus guitar","source":{"stream":"input"},"start":{"ref":{"id":"s2","prop":"end"}},"end":{"event":"button_press"}},{"id":"s4","source":{"ref":{"id":"s2"}},"start":{"ref":{"id":"s3","prop":"end"}}},{"id":"s5","source":{"ref":{"id":"s3"}},"start":{"ref":{"id":"s4","prop":"end"}}},{"id":"s6","name":"bridge guitar","source":{"stream":"input"},"start":{"ref":{"id":"s5","prop":"end"}},"end":{"event":"button_press"}},{"id":"s12","source":{"ref":{"id":"s3"}},"start":{"ref":{"id":"s6","prop":"end"}}}]},{"id":"t2","name":"bass","snippets":[{"id":"s7","name":"verse bass","source":{"stream":"input"},"start":{"ref":{"id":"s3","prop":"end"}},"dur":{"ref":{"id":"s2","prop":"dur"}}},{"id":"s8","name":"chorus bass","source":{"stream":"input"},"start":{"ref":{"id":"s7","prop":"end"}},"dur":{"ref":{"id":"s3","prop":"dur"}}},{"id":"s9","source":{"ref":{"id":"s8"}},"start":{"ref":{"id":"s6","prop":"end"}}}]},{"id":"t3","name":"lead guitar","snippets":[{"id":"s10","name":"chorus lead guitar","source":{"stream":"input"},"start":{"ref":{"id":"s6","prop":"end"}},"dur":{"ref":{"id":"s3","prop":"dur"}}}]}]},"version":"0.0"}`
-	program = Program.fromJSON(test_input, {
+	let options = {
 		mixerContainer: document.querySelector("#mixer"),
 		workspaceContainer: document.querySelector("#workspace"),
 		infoPanelContainer: document.querySelector("#info-panel")
-	});
+	};
+	if (input) {
+		program = Program.fromJSON(input, options);
+	} else {
+		program = new Program({ title: "untitled program" }, [], options);
+		program.addTrack("untitled track");
+	}
+	document.querySelector("#main-menu .open").onclick = () => {
+		// for some reason pywebview is still undefined when this script is loaded,
+		// which is why we have to wrap it in an anonymous function
+		pywebview.api.open().catch(error => alert(error));
+	};
 });
