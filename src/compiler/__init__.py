3e13d7358ab6f62999ef8ffd8da8c764781fde58
David Husz <contact@davidhusz.com>
2021-07-18 04:01:18 +0100

    Enable conversion to JSON

--- src/compiler/__init__.py
+++ src/compiler/__init__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 import strictyaml as yaml
-import re
+import json
+import ast
 import sys, argparse
 
 
@@ -9,11 +10,11 @@ class Program:
 	def __init__(self, attrs, tracks):
 		self.attrs = attrs
 		self.tracks = tracks
-		self.dereference_attrs()
 	
 	@classmethod
 	def fromYAML(cls, input):
 		parsed_input = yaml.load(input).data
+		version = parsed_input['version']
 		program_attrs = parsed_input['program']
 		tracks = []
 		for track_attrs in program_attrs.pop('tracks'):
@@ -21,7 +22,8 @@ class Program:
 			for snippet_attrs in track_attrs.pop('snippets'):
 				snippets.append(Snippet(snippet_attrs))
 			tracks.append(Track(track_attrs, snippets))
-		program = Program(program_attrs, tracks)
+		program = cls(program_attrs, tracks)
+		program.version = version
 		return program
 	
 	def get_snippet_by_id(self, id):
@@ -31,20 +33,6 @@ class Program:
 					return snippet
 		else:
 			raise IndexError(f'no snippet with id {id!r} exists')
-	
-	def dereference_attrs(self):
-		def replace(match):
-			ref_id, ref_attr = match.groups()
-			ref_name = self.get_snippet_by_id(ref_id).attrs['id']
-			return ref_name + ref_attr
-		for track in self.tracks:
-			for snippet in track.snippets:
-				for attr, val in snippet.attrs.items():
-					snippet.attrs[attr] = re.sub((
-						r'\$(\w+)'       # this group matches the reference id without the dollar sign
-						r'((?:\.\w+)*)'  # this group matches any number of attributes (such as '/end')
-						), replace, val
-					)
 	
 	def as_python(self):
 		title = 'program'
@@ -56,6 +44,12 @@ class Program:
 		content = '\n\n'.join(track.as_python(title) for track in self.tracks)
 		footer = f'\n\n{title}.start()'
 		return header + content + footer
+	
+	def as_dict(self):
+		return {**self.attrs, 'tracks': [track.as_dict() for track in self.tracks]}
+	
+	def as_json(self):
+		return json.dumps({'program': self.as_dict(), 'version': self.version})
 
 
 class Track:
@@ -69,27 +63,72 @@ class Track:
 		header = f"{id} = {program_name}.add_track({name})\n\n"
 		content = '\n\n'.join(snippet.as_python(id) for snippet in self.snippets)
 		return header + content
+	
+	def as_dict(self):
+		return {**self.attrs, 'snippets': [snippet.as_dict() for snippet in self.snippets]}
 
 
 class Snippet:
 	def __init__(self, attrs):
 		self.attrs = attrs
-		if self.attrs['source'] == 'input':
-			self.attrs['source'] = 'Input()'
-		def replace_event(event):
-			if event == 'button_press':
-				return 'events.ButtonPress()'
-			elif event == 'boot':
-				return 'events.Boot()'
-			elif event == 'shutdown':
-				return 'events.Shutdown()'
+	
+	def parse_expr(self, expr):
+		if type(expr) == ast.BinOp:
+			operators = {
+				ast.Add: 'add',
+				ast.Sub: 'sub',
+				ast.Mult: 'mul',
+				ast.Div: 'div'
+			}
+			return {
+				operators[type(expr.op)]: [
+					self.parse_expr(expr.left),
+					self.parse_expr(expr.right)
+				]
+			}
+		elif type(expr) == ast.Attribute:
+			return {
+				'ref': {
+					'id': expr.value.id,
+					'prop': expr.attr
+				}
+			}
+		elif type(expr) == ast.Name:
+			return {
+				'ref': {
+					'id': expr.id
+				}
+			}
+		elif type(expr) == ast.Constant:
+			return expr.value
+	
+	def convert_attrs(self, target):
+		if target == 'python':
+			conversions = {
+				'input': 'Input()',
+				'boot': 'events.Boot()',
+				'button_press': 'events.ButtonPress()'
+			}
+		elif target == 'json':
+			conversions = {
+				'input': {'stream': 'input'},
+				'boot': {'event': 'boot'},
+				'button_press': {'event': 'button_press'}
+			}
+		for attr, val in self.attrs.items():
+			if not '$' in val:
+				self.attrs[attr] = conversions.get(val, val)
 			else:
-				return event
-		for event in ['start', 'end']:
-			if event in self.attrs:
-				self.attrs[event] = replace_event(self.attrs[event])
+				pythonic = val.replace('$', '')
+				if target == 'python':
+					self.attrs[attr] = pythonic
+				elif target == 'json':
+					expr = ast.parse(pythonic, mode='eval').body
+					self.attrs[attr] = self.parse_expr(expr)
+			
 	
 	def as_python(self, track_name):
+		self.convert_attrs('python')
 		attrs = self.attrs.copy()  # we will be removing items, so we need a deep copy
 		id = attrs.pop('id')
 		if 'name' in attrs:
@@ -98,6 +137,10 @@ class Snippet:
 		args = ',\n\t'.join(f'{attr} = {val}' for attr, val in attrs.items())
 		output = f'{id} = {track_name}.add_snippet(\n\t{args}\n)'
 		return output
+	
+	def as_dict(self):
+		self.convert_attrs('json')
+		return self.attrs
 
 
 def laszlo2python(input):
@@ -105,9 +148,19 @@ def laszlo2python(input):
 	return program.as_python()
 
 
+def laszlo2json(input):
+	program = Program.fromYAML(input)
+	return program.as_json()
+
+
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument('input', nargs='?', default=sys.stdin, type=argparse.FileType('r'))
 	parser.add_argument('-o', '--output', default=sys.stdout, type=argparse.FileType('w'))
+	parser.add_argument('-t', '--to', default='python', metavar='format')
 	args = parser.parse_args()
-	print(laszlo2python(args.input.read()), file=args.output)
+	if args.to == 'python':
+		convert = laszlo2python
+	elif args.to == 'json':
+		convert = laszlo2json
+	print(convert(args.input.read()), file=args.output)
