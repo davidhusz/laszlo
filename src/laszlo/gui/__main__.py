85573b3abef56f713d2b401715d5aa1b015fe82a
David Husz <contact@davidhusz.com>
2021-07-26 05:41:50 +0100

    Add script for standalone binary creation

--- src/laszlo/gui/__main__.py
+++ src/laszlo/gui/__main__.py
@@ -1,11 +1,14 @@
 from . import open_editor, open_multiple_editors
 import argparse
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser('laszlo.gui')
+def main():
+    parser = argparse.ArgumentParser()
     parser.add_argument('input', nargs='*', default=None, type=argparse.FileType('r'))
     args = parser.parse_args()
     if args.input:
         open_multiple_editors(args.input)
     else:
         open_editor()
+
+if __name__ == '__main__':
+    main()
